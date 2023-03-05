9df4dfdbad9e756a662734a1f26e2869a51c4019
BTMuli <BT-Muli@outlook.com>
2023-03-05 15:03:50 +0800

    style(prettier): 添加 prettier 格式化代码

--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -6,16 +6,16 @@ const greetMsg = ref("");
 const name = ref("");
 
 async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsg.value = await invoke("greet", { name: name.value });
+	// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+	greetMsg.value = await invoke("greet", { name: name.value });
 }
 </script>
 
 <template>
-  <div class="card">
-    <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-    <button type="button" @click="greet()">Greet</button>
-  </div>
+	<div class="card">
+		<input id="greet-input" v-model="name" placeholder="Enter a name..." />
+		<button type="button" @click="greet()">Greet</button>
+	</div>
 
-  <p>{{ greetMsg }}</p>
+	<p>{{ greetMsg }}</p>
 </template>
