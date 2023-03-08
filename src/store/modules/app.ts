35f2e89b396088210dbb60ce9e97ae9af8c3f799
BTMuli <BT-Muli@outlook.com>
2023-03-08 22:05:27 +0800

    feat(merge): 新建 merge 目录用于存储列表渲染数据

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -1,8 +1,19 @@
+/**
+ * @file store modules app.ts
+ * @description App store module
+ * @author BTMuli<bt-muli@outlook.com>
+ * @since Alpha
+ */
+
 import { defineStore } from "pinia";
 import { path } from "@tauri-apps/api";
 
+// 用于存储原生数据的路径
 const appDataDir = `${await path.appLocalDataDir()}appData`;
+// 用于存储用户数据的路径
 const userDataDir = `${await path.appLocalDataDir()}userData`;
+// 用于存储合并数据的路径-列表渲染时使用，减少重复计算
+const mergeDataDir = `${await path.appLocalDataDir()}mergeData`;
 
 const useAppStore = defineStore({
 	id: "app",
@@ -18,11 +29,21 @@ const useAppStore = defineStore({
 			dataPath: {
 				app: appDataDir,
 				user: userDataDir,
+				merge: mergeDataDir,
 			},
 			// 应用数据路径
 			appPath: {
 				achievements: `${appDataDir}\\achievements.json`,
 				achievementSeries: `${appDataDir}\\achievementSeries.json`,
+			},
+			// 用户数据路径
+			userPath: {
+				achievements: `${userDataDir}\\achievements.json`,
+			},
+			// 合并数据路径
+			mergePath: {
+				achievements: `${mergeDataDir}\\achievements.json`,
+				achievementSeries: `${mergeDataDir}\\achievementSeries.json`,
 			},
 		};
 	},
@@ -39,11 +60,21 @@ const useAppStore = defineStore({
 			this.dataPath = {
 				app: appDataDir,
 				user: userDataDir,
+				merge: mergeDataDir,
 			};
 			// 初始化应用数据路径
 			this.appPath = {
 				achievements: `${appDataDir}\\achievements.json`,
 				achievementSeries: `${appDataDir}\\achievementSeries.json`,
+			};
+			// 初始化用户数据路径
+			this.userPath = {
+				achievements: `${userDataDir}\\achievements.json`,
+			};
+			// 初始化合并数据路径
+			this.mergePath = {
+				achievements: `${mergeDataDir}\\achievements.json`,
+				achievementSeries: `${mergeDataDir}\\achievementSeries.json`,
 			};
 		},
 	},
