7dae84e85abf83223a298add211a7b3e0a5e7923
BTMuli <BT-Muli@outlook.com>
2023-03-08 23:01:26 +0800

    fix(achievements): 两个新 interface 之前忘加了

--- src/interface/Achievements.ts
+++ src/interface/Achievements.ts
@@ -53,3 +53,49 @@ export interface AchievementSeries {
 	version?: string;
 	icon?: string;
 }
+
+/**
+ * @description 用于展示的成就系列类型-菜单栏左侧
+ * @interface AchievementSeriesDisplay
+ * @property {number} id - 成就系列 ID
+ * @property {number} order - 成就系列排列顺序，用于展示全部成就系列
+ * @property {string} name - 成就系列名称
+ * @property {AchievementDisplay[]} achievements - 成就系列包含的成就
+ * @property {number} total_count - 成就系列包含的成就数
+ * @property {number} completed_count - 成就系列已完成的成就数
+ * @property {number} card - 成就系列奖励，这边是名片 ID todo
+ * @property {string} icon - 成就系列图标 todo
+ * @return AchievementSeriesDisplay
+ */
+export interface AchievementSeriesDisplay {
+	id: number;
+	order: number;
+	name: string;
+	achievements: AchievementDisplay[];
+	total_count: number;
+	completed_count: number;
+	card?: number;
+	icon?: string;
+}
+
+/**
+ * @description 用于展示的成就类型-菜单栏右侧
+ * @interface AchievementDisplay
+ * @property {number} id - 成就 ID
+ * @property {number} order - 成就排列顺序，用于展示全部成就
+ * @property {string} name - 成就名称
+ * @property {string} description - 成就描述
+ * @property {number} reward - 成就奖励
+ * @property {boolean} completed - 成就是否已完成
+ * @property {string} completed_time - 成就完成时间
+ * @return AchievementDisplay
+ */
+export interface AchievementDisplay {
+	id: number;
+	order: number;
+	name: string;
+	description: string;
+	reward: number;
+	completed: boolean;
+	completed_time?: string;
+}
