734be29bddc51107e78b691bf3a03dbf3a7beaf3
BTMuli <BT-Muli@outlook.com>
2023-03-11 15:27:59 +0800

    feat(nameCard): 粗略爬了一下 0-117 的成就名片，转了一下格式跟名称，有部分缺漏的就是了

--- src/data/app/index.ts
+++ src/data/app/index.ts
@@ -7,7 +7,10 @@
 
 import achievements from "./achievements.json";
 import achievementSeries from "./achievementSeries.json";
+import nameCard from "./nameCard.json";
 import { Achievement, AchievementSeries } from "../../interface/Achievements";
+import { NameCard } from "../../interface/NameCard";
+import { Map } from "../../interface/Base";
 
 export const AppDataList = [
 	{
@@ -18,9 +21,14 @@ export const AppDataList = [
 		name: "achievementSeries.json",
 		data: achievementSeries as AchievementSeries[],
 	},
+	{
+		name: "nameCard.json",
+		data: nameCard as unknown as Map<NameCard>,
+	},
 ];
 
 export const AppData = {
 	achievements: achievements as Achievement[],
 	achievementSeries: achievementSeries as AchievementSeries[],
+	nameCard: nameCard as unknown as Map<NameCard>,
 };
