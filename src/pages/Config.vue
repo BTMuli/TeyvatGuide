b6d237d1e828d41b0b17e1b1a08790d8a3eeb156
BTMuli <BT-Muli@outlook.com>
2023-03-16 01:08:12 +0800

    fix(config): 完善设置页相关操作

--- src/pages/Config.vue
+++ src/pages/Config.vue
@@ -8,13 +8,13 @@
 				<v-list-item-title>打开用户数据目录</v-list-item-title>
 			</v-list-item>
 			<v-list-item @click="deleteData" prepend-icon="mdi-delete">
-				<v-list-item-title>删除用户数据</v-list-item-title>
+				<v-list-item-title>清除用户缓存</v-list-item-title>
 			</v-list-item>
 			<v-list-item @click="deleteTemp" prepend-icon="mdi-delete">
 				<v-list-item-title>删除临时数据</v-list-item-title>
 			</v-list-item>
 			<v-list-item @click="setDefaultConfig" prepend-icon="mdi-cog">
-				<v-list-item-title>恢复默认配置</v-list-item-title>
+				<v-list-item-title>初始化数据</v-list-item-title>
 			</v-list-item>
 		</v-list>
 	</v-card>
@@ -57,6 +57,8 @@ import useAppStore from "../store/modules/app";
 import useAchievementsStore from "../store/modules/achievements";
 import { dialog, fs } from "@tauri-apps/api";
 import { BaseDirectory } from "@tauri-apps/api/fs";
+import { getDataList } from "../data/init";
+import { WriteTGData } from "../utils/TGIndex";
 
 // Store
 const appStore = useAppStore();
@@ -88,13 +90,12 @@ async function deleteData() {
 			dir: BaseDirectory.AppLocalData,
 			recursive: true,
 		});
-		await fs.removeDir("mergeData", {
-			dir: BaseDirectory.AppLocalData,
-			recursive: true,
-		});
 		await fs.removeDir("tempData", {
 			dir: BaseDirectory.AppLocalData,
 			recursive: true,
+		});
+		getDataList.map(async item => {
+			await WriteTGData(item.name, item.data);
 		});
 		await dialog.message("用户数据已删除!");
 		await achievementsStore.init();
@@ -116,10 +117,13 @@ async function deleteTemp() {
 }
 // 恢复默认配置
 async function setDefaultConfig() {
-	const res = await dialog.confirm("确定要恢复默认配置吗?");
+	const res = await dialog.confirm("确定要初始化数据吗?");
 	if (res) {
 		await appStore.init();
-		await dialog.message("已恢复默认配置!");
+		await achievementsStore.init();
+		dialog.message("已恢复默认配置!").then(() => {
+			window.location.reload();
+		});
 	}
 }
 </script>
