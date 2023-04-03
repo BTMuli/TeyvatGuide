790b4dad2ba4f8a9b1caebc0f0546d4ebf65f5d1
BTMuli <BT-Muli@outlook.com>
2023-04-03 13:51:20 +0800

    fix(jsonViewer): type 变更

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,7 +7,7 @@ declare module "*.vue" {
 }
 
 declare module 'vue-json-viewer' {
-	import { Plugin } from 'vue';
-	const plugin: Plugin;
-	export default plugin;
+	import type { DefineComponent } from "vue";
+	const component: DefineComponent<{}, {}, any>;
+	export default component;
 }
