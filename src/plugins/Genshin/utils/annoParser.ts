21a0577ca28697e3d16ec50bd16bc9711dc1f066
BTMuli <BT-Muli@outlook.com>
2023-04-06 16:22:56 +0800

    🐞 fix(decode): 修复高危漏洞
    (cherry picked from commit 4fa1e26553ef32b122b13ce463fbda381244c1b5)

--- src/plugins/Genshin/utils/annoParser.ts
+++ src/plugins/Genshin/utils/annoParser.ts
@@ -60,19 +60,19 @@ export function parseAnnoContent (data: string): string {
 
 /**
  * @description 转义正则表达式
- * @since Alpha v0.1.1
+ * @since Alpha v0.1.2
  * @param {string} data 内容
  * @returns {string} 转义后的内容
  */
 export function decodeRegExp (data: string): string {
   let res = data;
   if (res.length === 0) return res;
-  res = res.replace(/&amp;/g, "&");
   res = res.replace(/&lt;/g, "<");
   res = res.replace(/&gt;/g, ">");
   res = res.replace(/&nbsp;/g, " ");
   res = res.replace(/&#39;/g, "'");
   res = res.replace(/&quot;/g, "\"");
   res = res.replace(/&apos;/g, "'");
+  res = res.replace(/&amp;/g, "&");
   return res;
 }
