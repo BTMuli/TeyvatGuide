7c7ee077caa3c07e54321b4cd4d1dc0a7faf9be4
BTMuli <BT-Muli@outlook.com>
2023-04-14 20:13:21 +0800

    💄 add(weapon): 先挂张图在那，json 跟资源明天再搞吧

--- src/pages/WIKI/Weapon.vue
+++ src/pages/WIKI/Weapon.vue
@@ -1,1 +1,7 @@
-<template />
+<template>
+  <TLoading empty title="开发中" loading="false" />
+</template>
+<script lang="ts" setup>
+// vue
+import TLoading from "../../components/t-loading.vue";
+</script>
