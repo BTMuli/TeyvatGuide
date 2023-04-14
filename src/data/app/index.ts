7047863448e698e98920fa796a8a617191b8c9a8
BTMuli <BT-Muli@outlook.com>
2023-04-14 18:10:42 +0800

    📦 add(character): 添加角色资源&Json

--- src/data/app/index.ts
+++ src/data/app/index.ts
@@ -2,15 +2,16 @@
  * @file data app index
  * @description data app index
  * @author BTMuli<bt-muli@outlook.com>
- * @since Alpha v0.1.3
+ * @since Alpha v0.1.
  */
 
 // Data
 import achievements from "./achievements.json";
 import achievementSeries from "./achievementSeries.json";
+import calendar from "./calendar.json";
+import character from "./character.json";
 import GCG from "./GCG.json";
 import nameCards from "./nameCards.json";
-import calendar from "./calendar.json";
 
 export const AppDataList = [
   {
@@ -22,6 +23,14 @@ export const AppDataList = [
     data: achievementSeries as Record<number, BTMuli.Genshin.AchievementSeries>,
   },
   {
+    name: "calendar.json",
+    data: calendar as Record<number, BTMuli.Genshin.Calendar.Data>,
+  },
+  {
+    name: "character.json",
+    data: character as BTMuli.Genshin.Wiki.Character.BriefInfo[],
+  },
+  {
     name: "GCG.json",
     data: GCG as BTMuli.Genshin.Wiki.GCG.BriefInfo[],
   },
@@ -29,16 +38,14 @@ export const AppDataList = [
     name: "nameCards.json",
     data: nameCards as Record<number, BTMuli.Genshin.NameCard[]>,
   },
-  {
-    name: "calendar.json",
-    data: calendar as Record<number, BTMuli.Genshin.Calendar.Data>,
-  },
+
 ];
 
 export const AppData = {
   achievements: achievements as Record<number, BTMuli.Genshin.Achievement>,
   achievementSeries: achievementSeries as Record<number, BTMuli.Genshin.AchievementSeries>,
+  calendar: calendar as Record<number, BTMuli.Genshin.Calendar.Data>,
+  character: character as BTMuli.Genshin.Wiki.Character.BriefInfo[],
   GCG: GCG as BTMuli.Genshin.Wiki.GCG.BriefInfo[],
   nameCards: nameCards as Record<number, BTMuli.Genshin.NameCard[]>,
-  calendar: calendar as Record<number, BTMuli.Genshin.Calendar.Data>,
 };
