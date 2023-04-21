add9fc4d23ec8a3bdf7172ba3fd3d824b31db6c3
BTMuli <BT-Muli@outlook.com>
2023-04-22 00:26:34 +0800

    🏷️ fix(json): 更正 type

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,7 @@
  * @file vite-env.d.ts
  * @description vite-env.d.ts
  * @author BTMuli<bt-muli@outlook.com>
- * @since Alpha v0.1.2
+ * @since Alpha v0.1.3
  */
 
 declare module "*.vue" {
@@ -11,8 +11,18 @@ declare module "*.vue" {
   export default component;
 }
 
+/**
+ * @description vue-json-viewer
+ * @package vue-json-viewer
+ * @version 3.0.4
+ * @todo 仅声明了用到的属性
+ */
 declare module "vue-json-viewer" {
   import type { DefineComponent } from "vue";
-  const component: DefineComponent<object, object, any>;
+  const component: DefineComponent<{
+    value: any
+    copyable: boolean
+    boxed: boolean
+  }>;
   export default component;
 }
