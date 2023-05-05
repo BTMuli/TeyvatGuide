592fe02d1482607ad5e31759b14757abfea7b08a
BTMuli <BT-Muli@outlook.com>
2023-05-05 18:02:59 +0800

    📦 add(salt): 添加盐值

---
+++ src/core/constant/TGConstant.ts
@@ -0,0 +1,14 @@
+/**
+ * @file core constant TGConstant.ts
+ * @description 常量
+ * @author BTMuli<bt-muli@outlook.com>
+ * @since Alpha v0.2.0
+ */
+
+import SALT from "./salt";
+
+const TGConstant = {
+  SALT,
+};
+
+export default TGConstant;
---
+++ src/core/constant/salt.ts
@@ -0,0 +1,24 @@
+/**
+ * @file core constant salt.ts
+ * @description 用到的 salt 值
+ * @author BTMuli<bt-muli@outlook.com>
+ * @since Alpha v0.2.0
+ */
+
+/**
+ * @description salt 值
+ * @version 2.49.1
+ * @since Alpha v0.2.0
+ */
+const SALT = {
+  BBS: {
+    K2: "egBrFMO1BPBG0UX5XOuuwMRLZKwTVKRV",
+    LK2: "DG8lqMyc9gquwAUFc7zBS62ijQRX9XF7",
+  },
+  Other: {
+    X4: "OYcKGMRDRpnluotQHWwPDnV4xZG4WRMx",
+    X6: "t0qEgfub6cvueAPgR5m9aQWWVciEer7v",
+  },
+};
+
+export default SALT;
