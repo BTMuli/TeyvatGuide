a27f6277061a8a523bf136e7382910f141168bf0
BTMuli <BT-Muli@outlook.com>
2023-05-19 22:52:48 +0800

    ✨ 素材日历 overlay 完工

--- src/components/t-calendar.vue
+++ src/components/t-calendar.vue
@@ -4,7 +4,8 @@
       <v-list-item-title style="color: #fec90b; margin-left: 10px; margin-bottom: 10px; font-family: Genshin, serif">
         <v-icon color="#EBD49E">
           mdi-calendar-clock
-        </v-icon> 今日素材
+        </v-icon>
+        今日素材
         <span style="color: #faf7e8">{{ dateNow }}</span>
         <v-btn
           v-for="text of btnText"
@@ -31,23 +32,9 @@
               v-for="item in characterCards"
               :key="item.id"
               class="card-box"
-              @click="showContent(item)"
+              @click="selectContent(item, 'character')"
             >
-              <div class="card-bg">
-                <img :src="item.bg" alt="bg">
-              </div>
-              <div class="card-icon">
-                <img :src="item.icon" alt="icon">
-              </div>
-              <div class="card-cover">
-                <div class="card-element">
-                  <img :src="item.element" alt="element">
-                </div>
-                <div class="card-name">
-                  <img :src="item.weapon_type" alt="weapon">
-                  <span>{{ item.name }}</span>
-                </div>
-              </div>
+              <TMiniAvatar size="100px" :model-value="item" />
             </div>
           </div>
         </div>
@@ -60,22 +47,9 @@
               v-for="item in weaponCards"
               :key="item.id"
               class="card-box"
-              @click="showContent(item)"
+              @click="selectContent(item, 'weapon')"
             >
-              <div class="card-bg">
-                <img :src="item.bg" alt="bg">
-              </div>
-              <div class="card-icon">
-                <img :src="item.icon" alt="icon">
-              </div>
-              <div class="card-cover">
-                <div class="card-type">
-                  <img :src="item.weapon_type" alt="element">
-                </div>
-                <div class="card-name">
-                  <span>{{ item.name }}</span>
-                </div>
-              </div>
+              <TMiniWeapon size="100px" :model-value="item" />
             </div>
           </div>
         </div>
@@ -84,11 +58,54 @@
     <v-snackbar v-model="snackbar" :timeout="1500" :color="snackbarColor">
       {{ snackbarText }}
     </v-snackbar>
+    <v-overlay v-model="showItem">
+      <div class="calendar-item-box">
+        <div class="calendar-item-top">
+          <div class="calendar-item-icon">
+            <Component :is="TMiniAvatar" v-if="selectedType=== 'character'" :model-value="selectedItem" size="100px" />
+            <Component :is="TMiniWeapon" v-if="selectedType=== 'weapon'" :model-value="selectedItem" size="100px" />
+          </div>
+          <div class="calendar-item-content">
+            <div v-for="item in selectedItem.materials" class="calendar-item-sub">
+              <TCalendarMaterial :item="item" />
+            </div>
+          </div>
+        </div>
+        <div class="calendar-item-line">
+          <img src="/source/UI/item-line.webp" alt="line">
+        </div>
+        <div class="calendar-item-bottom">
+          <div class="calendar-item-source">
+            <div class="calendar-source-text">
+              来源：
+            </div>
+            <img :src="`/icon/nation/${selectedItem.source.area}.webp`" alt="icon">
+            <div class="calendar-source-text">
+              {{ selectedItem.source.area }} - {{ selectedItem.source.name }}
+            </div>
+          </div>
+          <div class="detail-btn">
+            <v-btn @click="showDetail(selectedItem)">
+              <template #append>
+                <img src="../assets/icons/arrow-right.svg" alt="right">
+              </template>
+              详情
+            </v-btn>
+          </div>
+        </div>
+      </div>
+      <div class="calendar-item-close" @click="showItem = false">
+        <v-icon>mdi-close</v-icon>
+      </div>
+    </v-overlay>
   </v-list>
 </template>
 <script lang="ts" setup>
 // vue
-import { ref, onMounted } from "vue";
+import { onMounted, ref } from "vue";
+import TMiniAvatar from "./t-mini-avatar.vue";
+import TMiniWeapon from "./t-mini-weapon.vue";
+import TCalendarMaterial from "./t-calendar-material.vue";
 // data
 import { TGAppData } from "../data";
 // interface
@@ -103,9 +120,16 @@ const calendarData = ref(TGAppData.calendar);
 const weekNow = ref(0 as number);
 const btnNow = ref(0 as number);
 const dateNow = ref(new Date().toLocaleDateString());
+
+// calendar
 const calendarNow = ref([] as BTMuli.Genshin.Calendar.Data[]);
-const characterCards = ref({} as Record<number, BTMuli.Genshin.Calendar.Data>);
-const weaponCards = ref({} as Record<number, BTMuli.Genshin.Calendar.Data>);
+const characterCards = ref([] as BTMuli.Genshin.Calendar.Data[]);
+const weaponCards = ref([] as BTMuli.Genshin.Calendar.Data[]);
+
+// calendar item
+const showItem = ref(false as boolean);
+const selectedItem = ref({} as BTMuli.Genshin.Calendar.Data);
+const selectedType = ref("character");
 
 // snackbar
 const snackbar = ref(false as boolean);
@@ -163,7 +187,13 @@ function getCalendar (day: number) {
   return calendarData.value.filter((item) => item.drop_day.includes(day));
 }
 
-function showContent (item: BTMuli.Genshin.Calendar.Data) {
+function selectContent (item: BTMuli.Genshin.Calendar.Data, type: string) {
+  selectedItem.value = item;
+  selectedType.value = type;
+  showItem.value = true;
+}
+
+function showDetail (item: BTMuli.Genshin.Calendar.Data) {
   if (item.content_id === null) {
     snackbarText.value = "暂无详情";
     snackbarColor.value = "error";
@@ -182,157 +212,169 @@ function getContents (day: number) {
 }
 </script>
 <style lang="css" scoped>
+/* calendar 大盒子 */
 .calendar-card {
-  margin-top: 10px;
-  font-family: Genshin-Light, serif;
-  background: var(--content-bg-1);
-  border-radius: 10px;
+    margin-top: 10px;
+    font-family: Genshin-Light, serif;
+    background: var(--content-bg-1);
+    border-radius: 10px;
 }
 
 .calendar-btn {
-  margin-left: 10px;
-  font-family: Genshin-Light, serif;
-  color: var(--btn-text-1);
-  border-radius: 10px;
+    margin-left: 10px;
+    font-family: Genshin-Light, serif;
+    color: var(--btn-text-1);
+    border-radius: 10px;
 }
 
 .calendar-box {
-  margin:5px;
+    margin: 5px;
 }
 
 .calendar-single {
-  margin-bottom: 10px;
-  background: var(--content-bg-2);
-  color: var(--content-bg-1);
-  border-radius: 10px;
+    margin-bottom: 10px;
+    background: var(--content-bg-2);
+    color: var(--content-bg-1);
+    border-radius: 10px;
 }
 
 .calendar-title {
-  font-size: 1.5rem;
-  font-family: Genshin, serif;
-  color: #546D8B;
-  padding-left: 15px;
-  padding-top: 10px;
+    font-size: 1.5rem;
+    font-family: Genshin, serif;
+    color: #546D8B;
+    padding-left: 15px;
+    padding-top: 10px;
 }
 
 .cards-grid {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
-  grid-gap: 10px;
-  padding: 10px;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+    grid-gap: 10px;
+    padding: 10px;
 }
 
-.card-box {
-  position: relative;
-  width: 100px;
+/* overlay 盒子 */
+.calendar-item-box {
+    position: absolute;
+    width: 440px;
+    height: 200px;
+    top: calc(50vh - 100px);
+    left: calc(50vw - 220px);
+    background: var(--content-bg-2);
+    border-radius: 10px;
+    padding: 10px;
+    align-items: center;
+}
+
+.calendar-item-top {
   height: 100px;
-  cursor: pointer;
+  width: 100%;
+  display: flex;
 }
 
-.card-bg {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+.calendar-item-icon {
+    height: 100px;
+    width: 100px;
+}
+
+.calendar-item-content {
+    margin-left: 10px;
+    font-family: Genshin, serif;
+    color: var(--content-bg-1);
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 10px;
+		row-gap: 10px;
+}
+
+.calendar-item-sub {
+  width: 150px;
+  height: 45px;
+	border-radius: 10px;
+}
+
+.calendar-item-sub img {
+  width: 40px;
+  height: 40px;
+  object-fit: cover;
+  border-radius: 10px;
+}
+
+.calendar-item-line {
+    width: 420px;
+}
+
+.calendar-item-line img {
+    width: 100%;
+    height: auto;
+}
+
+.calendar-item-bottom {
+  background: rgb(0 0 0 / 30%);
+  padding: 3px 10px;
+  width: 420px;
+  height: 56px;
   border-radius: 5px;
-  overflow: hidden;
-}
-
-.card-bg img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-.card-icon {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  border-radius: 5px;
-}
-
-.card-icon img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-.card-cover {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border-radius: 5px;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
-
-.card-element {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 30px;
-  height: 30px;
-  display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 }
 
-.card-element img {
-  width: 20px;
-  height: 20px;
-  object-fit: cover;
+.calendar-item-bottom img {
+  width: 50px;
+  height: 50px;
 }
 
-.card-weapon img {
-  width: 20px;
-  height: 20px;
-  object-fit: cover;
-}
-
-.card-type {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 30px;
-  height: 30px;
+.calendar-item-source {
+  width: 300px;
+  height: 50px;
   display: flex;
-  justify-content: center;
+  justify-content: left;
   align-items: center;
 }
 
-.card-type img {
-  width: 20px;
-  height: 20px;
-  object-fit: cover;
-}
-
-.card-name {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 30px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: rgb(20 20 20 / 50%);
-  border-bottom-left-radius: 5px;
-  border-bottom-right-radius: 5px;
+.calendar-source-text {
+  height: 50px;
   color: #fff;
-  font-size: 8px;
-  text-shadow: 0 0 5px #000;
-  font-family: Genshin, serif;
+  font-size: 20px;
+  font-family: Genshin-Light, serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.card-name img {
-  width: 20px;
-  height: 20px;
-  margin-right: 5px;
+.detail-btn {
+  font-family: Genshin, serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  border-radius: 5px;
+  transition: all 0.3s linear;
+}
+
+.detail-btn button {
+  background: var(--btn-bg-1);
+  color: #faf7e8;
+}
+
+.detail-btn button img {
+  width: 18px;
+  height: 18px;
+}
+
+.calendar-item-close {
+    position: absolute;
+    top: calc(50vh + 120px);
+    left: calc(50vw - 15px);
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    background: var(--content-bg-2);
+    color: var(--calendar-item-btn);
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
