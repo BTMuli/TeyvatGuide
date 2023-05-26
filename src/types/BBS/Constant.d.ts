c91e2f80e989af14d10906798bd755ff765b2b3b
BTMuli <BT-Muli@outlook.com>
2023-05-26 22:29:16 +0800

    🏷️ 添加常用 ck 组合

--- src/types/BBS/Constant.d.ts
+++ src/types/BBS/Constant.d.ts
@@ -2,7 +2,7 @@
  * @file types BBS Constant.d.ts
  * @description BBS 常量相关类型定义文件
  * @author BTMuli<bt-muli@outlook.com>
- * @since Alpha v0.1.5
+ * @since Alpha v0.1.6
  */
 
 declare namespace TGApp.BBS.Constant {
@@ -45,4 +45,54 @@ declare namespace TGApp.BBS.Constant {
     stuid: string
     stoken_v2?: string
   }
+
+  /**
+   * @description cookie 组合-1
+   * @interface CookieGroup1
+   * @since Alpha v0.1.6
+   * @property {string} login_ticket 登录凭证
+   * @property {string} login_uid 登录 uid
+   * @return CookieGroup1
+   */
+  export interface CookieGroup1 {
+    login_ticket: string
+    login_uid: string
+  }
+
+  /**
+   * @description cookie 组合-2
+   * @interface CookieGroup2
+   * @since Alpha v0.1.6
+   * @property {string} account_id 账号 id
+   * @property {string} cookie_token cookie token
+   * @return CookieGroup2
+   */
+  export interface CookieGroup2 {
+    account_id: string
+    cookie_token: string
+  }
+
+  /**
+   * @description cookie 组合-3
+   * @interface CookieGroup3
+   * @since Alpha v0.1.6
+   * @property {string} ltoken ltoken
+   * @property {string} ltuid ltoken 对应的 uid
+   * @return CookieGroup3
+   */
+  export interface CookieGroup3 {
+    ltoken: string
+    ltuid: string
+  }
+
+  /**
+   * @description cookie 组合-4
+   * @interface CookieGroup4
+   * @since Alpha v0.1.6
+   * @extends CookieGroup2
+   * @extends CookieGroup3
+   * @return CookieGroup4
+   */
+  export interface CookieGroup4 extends CookieGroup2, CookieGroup3 {}
+
 }
