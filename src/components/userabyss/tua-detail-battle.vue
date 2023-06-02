bc497491953f363d03506c9cf3d06c1e1fd01442
BTMuli <BT-Muli@outlook.com>
2023-06-02 19:42:02 +0800

    💄 微调布局

--- src/components/userabyss/tua-detail-battle.vue
+++ src/components/userabyss/tua-detail-battle.vue
@@ -1,19 +1,19 @@
 <template>
+  <div class="tud-db-time">
+    {{ props.modelValue.time }}
+  </div>
   <div class="tud-db-icons-grid">
     <TibAbyssDetail
       v-for="avatar in props.modelValue.characters"
       :key="avatar.id" :model-value="avatar"
     />
   </div>
-  <div class="tud-db-time">
-    {{ props.modelValue.time }}
-  </div>
 </template>
 <script lang="ts" setup>
 import TibAbyssDetail from "../itembox/tib-abyss-detail.vue";
 
 interface TuaDetailBattleProps {
-  modelValue: TGApp.Sqlite.Abyss.Battle
+  modelValue: TGApp.Sqlite.Abyss.Battle;
 }
 
 const props = defineProps<TuaDetailBattleProps>();
