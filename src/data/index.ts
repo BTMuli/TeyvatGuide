9ffd0468edce837d3a5d8de838907d4f3648f2bb
BTMuli <BT-Muli@outlook.com>
2023-08-15 17:25:47 +0800

    🔥 删除无用 json

--- src/data/index.ts
+++ src/data/index.ts
@@ -11,7 +11,6 @@ import achievementSeries from "./app/achievementSeries.json";
 import calendar from "./app/calendar.json";
 import character from "./app/character.json";
 import GCG from "./app/GCG.json";
-import material from "./app/material.json";
 import nameCards from "./app/namecard.json";
 import weapon from "./app/weapon.json";
 
@@ -20,7 +19,6 @@ export const AppAchievementSeriesData: TGApp.App.Achievement.Series[] = achievem
 export const AppCalendarData: TGApp.App.Calendar.Item[] = calendar;
 export const AppCharacterData: TGApp.App.Character.WikiBriefInfo[] = character;
 export const AppGCGData: TGApp.App.GCG.WikiBriefInfo[] = GCG;
-export const AppMaterialData: TGApp.App.Calendar.Material[] = material;
 export const AppNameCardsData: TGApp.App.NameCard.Item[] = nameCards;
 export const AppWeaponData: TGApp.App.Weapon.WikiBriefInfo[] = weapon;
 
