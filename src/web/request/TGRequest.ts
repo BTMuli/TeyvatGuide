6e22eddcf5d5319545890140db78fb330a6c06c0
BTMuli <BT-Muli@outlook.com>
2023-08-28 10:33:44 +0800

    🔥 删除实时便笺请求

--- src/web/request/TGRequest.ts
+++ src/web/request/TGRequest.ts
@@ -2,13 +2,12 @@
  * @file web request TGRequest.ts
  * @description 应用用到的请求函数
  * @author BTMuli <bt-muli@outlook.com>
- * @since Alpha v0.2.2
+ * @since Alpha v0.2.3
  */
 
 import { getAbyss } from "./getAbyss";
 import { getAnnoList, getAnnoContent } from "./getAnno";
 import { getCookieTokenBySToken } from "./getCookieToken";
-import { getDailyNotes, getGeeTest, postGeeTest } from "./getDailyNotes";
 // import * from "./getEnkaData.ts";
 import { getGameAccountsBySToken, getGameAccountsByCookie } from "./getGameAccounts";
 import { getGameRecord } from "./getGameRecord";
@@ -51,11 +50,6 @@ const TGRequest = {
     calculate: {
       getSyncAvatarListAll,
       getSyncAvatarDetail,
-    },
-    dailyNote: {
-      widget: getDailyNotes,
-      getTest: getGeeTest,
-      postRes: postGeeTest,
     },
   },
 };
