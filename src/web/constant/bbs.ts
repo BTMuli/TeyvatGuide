1c69cf07a581fbfffb931f732070cb9de1c89d31
BTMuli <BT-Muli@outlook.com>
2023-11-16 22:01:10 +0800

    ⬆️ 米社 salt 更新，2.59.1 → 2.63.1

--- src/web/constant/bbs.ts
+++ src/web/constant/bbs.ts
@@ -4,19 +4,19 @@
  * @since Beta v0.3.6
  */
 
-export const BBS_VERSION = "2.59.1";
+export const BBS_VERSION = "2.63.1";
 export const BBS_UA_PC = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) miHoYoBBS/${BBS_VERSION}`;
 export const BBS_UA_MOBILE = `Mozilla/5.0 (Linux; Android 12) Mobile miHoYoBBS/${BBS_VERSION}`;
 export const BBS_APP_ID = "bll8iq97cem8";
 
 /**
  * @description salt 值
- * @version 2.59.1
- * @since Beta v0.3.3
+ * @version 2.63.1
+ * @since Beta v0.3.6
  */
 export const BBS_SALT = {
-  K2: "awFeNNTsLYcK20LSO60Es8CRVZOjCB1b",
-  LK2: "6pNd5NnDnbwKxewrPwEoWlSYwhualS2H",
+  K2: "BIPaooxbWZW02fGHZL1If26mYCljPgst",
+  LK2: "9ttJY72HxbjwWRNHJvn0n2AYue47nYsK",
   X4: "xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs",
   X6: "t0qEgfub6cvueAPgR5m9aQWWVciEer7v",
   PROD: "t0qEgfub6cvueAPgR5m9aQWWVciEer7v",
