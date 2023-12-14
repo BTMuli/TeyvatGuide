125ecee94324957602e31150093e5f438dff746b
BTMuli <BT-Muli@outlook.com>
2023-12-14 17:58:54 +0800

    🌱 图鉴页面开始动工？

--- src/views/WIKI/t-weapon.vue
+++ src/views/WIKI/t-weapon.vue
@@ -1,1 +1,25 @@
-<template />
+<template></template>
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+
+import showSnackbar from "../../components/func/snackbar";
+import { getWikiData } from "../../data";
+
+const id = <string>useRoute().params.id;
+const data = ref<any>();
+
+onMounted(async () => {
+  try {
+    const res = await getWikiData("Weapon", id);
+    data.value = res.default;
+  } catch (e) {
+    console.error(e);
+    showSnackbar({
+      text: "Wiki 数据获取失败",
+      color: "error",
+    });
+    history.back();
+  }
+});
+</script>
