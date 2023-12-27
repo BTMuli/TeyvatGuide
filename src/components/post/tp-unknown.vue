6b4cd2a6662fdf84c5289ff68cc6122667b63ed0
BTMuli <BT-Muli@outlook.com>
2023-12-27 23:13:25 +0800

    💄 完善 unknown 样式&处理

--- src/components/post/tp-unknown.vue
+++ src/components/post/tp-unknown.vue
@@ -1,12 +1,32 @@
 <template>
-  <div class="mys-post-unknown">
-    <code class="mys-post-unknown-code">{{ JSON.stringify(props.data, null, 2) }}</code>
+  <div class="tp-unknown-box">
+    <code class="tp-unknown-code">{{ JSON.stringify(props.data, null, 2) }}</code>
   </div>
 </template>
 <script lang="ts" setup>
+import { toRaw } from "vue";
+
 interface TpUnknownProps {
   data: TGApp.Plugins.Mys.SctPost.Empty;
 }
 
 const props = defineProps<TpUnknownProps>();
+
+console.warn("tpUnknown", toRaw(props.data.insert));
 </script>
+<style lang="css" scoped>
+.tp-unknown-box {
+  width: 800px;
+  padding: 10px;
+  border: 1px solid var(--common-shadow-1);
+  border-radius: 10px;
+  background: var(--box-bg-1);
+}
+
+.tp-unknown-code {
+  font-family: var(--font-text);
+  opacity: 0.6;
+  white-space: pre-wrap;
+  word-break: break-all;
+}
+</style>
