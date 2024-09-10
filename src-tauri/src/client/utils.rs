da74cbe3920b65e3e527835fd8609bb1ddf51996
目棃 <BT-Muli@outlook.com>
2024-09-10 19:46:26 +0800

    🐛 修复子窗口分辨率适应异常

--- src-tauri/src/client/utils.rs
+++ src-tauri/src/client/utils.rs
@@ -1,6 +1,6 @@
 //! @file src/client/utils.rs
 //! @desc 结合屏幕分辨率获取窗口大小
-//! @since Beta v0.5.2
+//! @since Beta v0.5.5
 
 use tauri::{AppHandle, Manager, Monitor};
 
@@ -14,9 +14,10 @@ pub fn get_window_size2(monitor: Monitor, width: f64, height: f64) -> (f64, f64)
   let monitor_size = monitor.size();
   let monitor_width = monitor_size.width as f64;
   let monitor_height = monitor_size.height as f64;
+  let monitor_scale = monitor.scale_factor();
   let width_scale = monitor_width / 1920.0;
   let height_scale = monitor_height / 1080.0;
-  let get_width = (width * width_scale).round();
-  let get_height = (height * height_scale).round();
+  let get_width = (width * width_scale / monitor_scale).round();
+  let get_height = (height * height_scale / monitor_scale).round();
   (get_width, get_height)
 }
