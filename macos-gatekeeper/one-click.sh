e4f2784fee1598e044339ccdb868c3ed619d5977
Lucas04-nhr <lucas04@foxmail.com>
2024-10-06 22:59:08 +0800

    Fix language-specific messages in one-click.sh

--- macos-gatekeeper/one-click.sh
+++ macos-gatekeeper/one-click.sh
@@ -22,18 +22,44 @@ NC="\033[0m"
 appName="TeyvatGuide.app"
 appBashName=${appName// /\ }
 appDIR="/Applications/${appBashName}"
-echo -e "『${RED}${appBashName%.*} ${NC}已损坏,无法打开/来自身份不明的开发者』等问题修复工具"
-echo ""
-#未安装APP时提醒安装，已安装绕过公证
-if [ ! -d "$appDIR" ];then
-  echo -e "❌ ${LIGHT_RED}您还未安装 ${appBashName%.*}, 请先将软件拷贝到【${NC}${LIGHT_BLUE}应用程序${NC}${LIGHT_RED}】目录${NC}"
-  else
-  #绕过公证
-  echo -e "👇 ${LIGHT_RED}请输入管理员密码（一般情况下就是开机密码）, 输入完成后按下回车键(输入过程中密码是看不见的)${NC}"
-  sudo spctl --master-disable
-  sudo /usr/bin/xattr -rd com.apple.quarantine /Applications/"$appBashName"
+
+# Check system language
+LANGUAGE=$(defaults read -g AppleLocale | cut -d "_" -f 1)
+
+if [ "$LANGUAGE" == "en" ]; then
+
+  echo -e "Fix tool of 『${RED}${appBashName%.*} ${NC}is damaged and cannot be opened / from an unidentified developer』"
   echo ""
-  echo -e "😁 ${LIGHT_GREEN}修复成功！${NC}您现在可以正常运行 ${LIGHT_GREEN}${appBashName%.*} ${NC}了。"
+  # Prompt to install the app if not installed, bypass notarization if installed
+  if [ ! -d "$appDIR" ];then
+    echo -e "❌ ${LIGHT_RED}You have not installed ${appBashName%.*}, please copy the software to the 【${NC}${LIGHT_BLUE}Applications${NC}${LIGHT_RED}】 directory${NC}"
+  else
+    # Bypass notarization
+    echo -e "👇 ${LIGHT_RED}Please enter the administrator password (usually the boot password), press Enter after entering (the password is invisible during input)${NC}"
+    sudo spctl --master-disable
+    sudo /usr/bin/xattr -rd com.apple.quarantine /Applications/"$appBashName"
+    echo ""
+    echo -e "😁 ${LIGHT_GREEN}Repair successful!${NC} You can now run ${LIGHT_GREEN}${appBashName%.*} ${NC}normally."
+  fi
+  echo ""
+  echo -e "✌️  The operation has been successfully completed, you can close this window now!"
+
+else
+
+  echo -e "『${RED}${appBashName%.*} ${NC}已损坏,无法打开/来自身份不明的开发者』等问题修复工具"
+  echo ""
+  #未安装APP时提醒安装，已安装绕过公证
+  if [ ! -d "$appDIR" ];then
+    echo -e "❌ ${LIGHT_RED}您还未安装 ${appBashName%.*}, 请先将软件拷贝到【${NC}${LIGHT_BLUE}应用程序${NC}${LIGHT_RED}】目录${NC}"
+  else
+    #绕过公证
+    echo -e "👇 ${LIGHT_RED}请输入管理员密码（一般情况下就是开机密码）, 输入完成后按下回车键(输入过程中密码是看不见的)${NC}"
+    sudo spctl --master-disable
+    sudo /usr/bin/xattr -rd com.apple.quarantine /Applications/"$appBashName"
+    echo ""
+    echo -e "😁 ${LIGHT_GREEN}修复成功！${NC}您现在可以正常运行 ${LIGHT_GREEN}${appBashName%.*} ${NC}了。"
+  fi
+  echo ""
+  echo -e "✌️  操作已成功完成, 本窗口可以关闭啦！"
+
 fi
-echo ""
-echo -e "✌️  操作已成功完成, 本窗口可以关闭啦！"
