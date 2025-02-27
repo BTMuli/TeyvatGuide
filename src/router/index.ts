aa6c75e59f9e79c595b05d6acba5eb82ccbefd6a
目棃 <BT-Muli@outlook.com>
2025-02-27 23:05:21 +0800

    🐛 修复路由跳转不生效

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 /**
  * @file router index.ts
  * @description 路由入口
- * @since Beta v0.6.8
+ * @since Beta v0.7.0
  */
 
 import { createRouter, createWebHistory } from "vue-router";
@@ -9,11 +9,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import routes from "./routes.js";
 
 const router = createRouter({ history: createWebHistory(), routes: routes });
+// 只有在特定页面忽略参数变化
+const ignoreRoutes: ReadonlyArray<string> = ["酒馆", "话题"];
 
 // 解决路由重复问题
 router.afterEach((to, from) => {
   if (from.name === to.name) {
-    if (from.query !== to.query) return;
+    if (from.query !== to.query && ignoreRoutes.includes(from.name?.toString() ?? "")) return;
     window.location.reload();
   }
 });
