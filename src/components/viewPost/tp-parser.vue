8f9006b9f4a96b5032823a406b2b148262996d25
目棃 <BT-Muli@outlook.com>
2025-03-08 22:51:25 +0800

    👔 调整判断逻辑

--- src/components/viewPost/tp-parser.vue
+++ src/components/viewPost/tp-parser.vue
@@ -95,8 +95,9 @@ function getTpName(tp: TGApp.BBS.SctPost.Base): Component {
   if (typeof tp.insert === "string") return TpText;
   // game_user_info属于backup_text的一种，必须放在backup_text判断的前面
   if ("game_user_info" in tp.insert) return TpUid;
-  if ("backup_text" in tp.insert) return TpBackupText;
+  // custom_emoticon属于backup_text的一种，必须放在backup_text判断的前面
   if ("custom_emoticon" in tp.insert) return TpEmoticon;
+  if ("backup_text" in tp.insert) return TpBackupText;
   if ("divider" in tp.insert) return TpDivider;
   if ("image" in tp.insert) return TpImage;
   if ("link_card" in tp.insert) return TpLinkCard;
