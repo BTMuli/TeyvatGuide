e5d3ade44c52a1bb2c72a809dc5a90608b223472
BTMuli <bt-muli@outlook.com>
2026-04-21 11:55:58 +0800

    🐛 修正危战验证Schema

--- src/plugins/Hutao/types/Challenge.d.ts
+++ src/plugins/Hutao/types/Challenge.d.ts
@@ -1,22 +1,16 @@
 /**
  * 幽境危战
- * @since Beta v0.9.9
+ * @since Beta v0.10.1
  */
 
 declare namespace TGApp.Plugins.Hutao.Challenge {
   /**
    * 导入数据结构
-   * @since Beta v0.9.9
+   * @since Beta v0.10.1
    */
   type ImportData = {
     uid: string;
     schedule_id: number;
-    start_time: string;
-    end_time: string;
-    name: string;
-    single: TGApp.Game.Challenge.Challenge;
-    mp: TGApp.Game.Challenge.Challenge;
-    blings: TGApp.Game.Challenge.ChallengeBlings;
     data: TGApp.Game.Challenge.ChallengeItem;
   };
 }
