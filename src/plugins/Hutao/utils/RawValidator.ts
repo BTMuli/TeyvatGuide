829348311450b84c7cfdc12e5bde44252a138878
BTMuli <bt-muli@outlook.com>
2026-04-21 11:57:36 +0800

    🔊 完善验证输出

--- src/plugins/Hutao/utils/RawValidator.ts
+++ src/plugins/Hutao/utils/RawValidator.ts
@@ -38,16 +38,17 @@ function validateAbyss(data: unknown): data is TGApp.Plugins.Hutao.Abyss.ImportD
 
 /**
  * 验证危战单条数据
- * @since Beta v0.9.9
+ * @since Beta v0.10.1
  * @param data - 待验证的数据
  * @returns 验证是否通过（类型收束）
  */
 function validateChallenge(data: unknown): data is TGApp.Plugins.Hutao.Challenge.ImportData {
   if (!ChallengeValidate(data)) {
-    const error = ChallengeValidate.errors?.[0];
-    if (error) {
+    if (ChallengeValidate.errors) {
+      const error = ChallengeValidate.errors[0];
+      console.error(ChallengeValidate.errors);
       showSnackbar.error(
-        `危战数据验证失败：${error.instancePath || error.schemaPath} ${error.message}`,
+        `深渊数据验证失败：${error.instancePath || error.schemaPath} ${error.message}`,
       );
     }
     return false;
@@ -57,16 +58,17 @@ function validateChallenge(data: unknown): data is TGApp.Plugins.Hutao.Challenge
 
 /**
  * 验证剧诗单条数据
- * @since Beta v0.9.9
+ * @since Beta v0.10.1
  * @param data - 待验证的数据
  * @returns 验证是否通过（类型收束）
  */
 function validateCombat(data: unknown): data is TGApp.Plugins.Hutao.Combat.ImportData {
   if (!CombatValidate(data)) {
-    const error = CombatValidate.errors?.[0];
-    if (error) {
+    if (CombatValidate.errors) {
+      const error = CombatValidate.errors[0];
+      console.error(CombatValidate.errors);
       showSnackbar.error(
-        `剧诗数据验证失败：${error.instancePath || error.schemaPath} ${error.message}`,
+        `深渊数据验证失败：${error.instancePath || error.schemaPath} ${error.message}`,
       );
     }
     return false;
