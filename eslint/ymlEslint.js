fc2534bbf6a6b65e400f5eac10d93ace6da4be09
BTMuli <bt-muli@outlook.com>
2026-05-19 23:19:07 +0800

    🔧 更新 ESLint 配置以支持更多 YAML 文件

--- eslint/ymlEslint.js
+++ eslint/ymlEslint.js
@@ -1,13 +1,13 @@
 /**
  * YAML 文件的 ESLint 配置
- * @since Beta v0.9.3
+ * @since Beta v0.10.2
  */
 import pluginYml from "eslint-plugin-yml";
 import * as ymlParser from "yaml-eslint-parser";
 
 const eslintConfigYml = [
   {
-    files: ["**/*.yml", "**/*.yaml"],
+    files: ["**/*.yml", "**/*.yaml", "pnpm-workspace.yaml", ".stylelintrc.yml", ".prettierrc.yml"],
     plugins: { yml: pluginYml },
     languageOptions: {
       parser: ymlParser,
