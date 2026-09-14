c742f63fa918a1da3b3946322e0c5cfa76ebcb31
BTMuli <bt-muli@outlook.com>
2026-09-14 17:20:08 +0800

    ⬆️ 更新依赖

--- src/utils/TGBbs.ts
+++ src/utils/TGBbs.ts
@@ -1,22 +1,22 @@
 /**
  * 关于 BBS 的工具函数
- * @since Beta v0.12.1
+ * @since Beta v0.12.2
  */
 
 import bbsEnum from "@enum/bbs.js";
 
-const BBS_VERSION: Readonly<string> = "2.114.0";
+const BBS_VERSION: Readonly<string> = "2.115.0";
 const BBS_UA_MOBILE: Readonly<string> = `Mozilla/5.0 (Linux; Android 12) Mobile miHoYoBBS/${BBS_VERSION}`;
 const BBS_UA_PC: Readonly<string> = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) miHoYoBBS/${BBS_VERSION}`;
 
 /**
  * salt 值
- * @since Beta v0.12.1
- * @remarks 2.114.0
+ * @since Beta v0.12.2
+ * @remarks 2.115.0
  */
 const BBS_SALT: Readonly<Record<TGApp.BBS.Auth.SaltKeyEnum, string>> = {
-  [bbsEnum.saltKey.K2]: "d64014da690671f8704695e993130f4c",
-  [bbsEnum.saltKey.LK2]: "21d2764ed385827b2005dc5d38b2a844",
+  [bbsEnum.saltKey.K2]: "09d39d16528ca0e4900a39ecc07d5062",
+  [bbsEnum.saltKey.LK2]: "b99b74d9023a6511231be2c7209b6fdb",
   [bbsEnum.saltKey.X4]: "xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs",
   [bbsEnum.saltKey.X6]: "t0qEgfub6cvueAPgR5m9aQWWVciEer7v",
   [bbsEnum.saltKey.PROD]: "t0qEgfub6cvueAPgR5m9aQWWVciEer7v",
